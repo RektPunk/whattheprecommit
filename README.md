8ad64ac100d4ddf684725f0c2ebe7099320cf91d
RektPunk <rektpunk@gmail.com>
2026-03-26 22:43:13 +0900

    [Feature] make cli avaliable (#1)

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # whattheprecommit
 Automatically ruins git commit messages with whatthecommit API. My final contribution to the team.
 
+## Installation
+### As a pre-commit hook
+Add this to your `.pre-commit-config.yaml` to kill professional commit messages forever:
 ```yaml
-# .pre-commit-config.yaml
 default_install_hook_types: [pre-commit, prepare-commit-msg]
 
 repos:
   - repo: https://github.com/RektPunk/whattheprecommit
-    rev: v0.0.1
+    rev: v0.0.2
     hooks:
       - id: whattheprecommit
 ```
+### Globally as a CLI
+To ruin your commit messages globally:
+```bash
+cargo install --git https://github.com/RektPunk/whattheprecommit
+```
+Just type `wtc` instead of `git commit -m "..."`. It's like Russian Roulette for your Git history.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use rand::prelude::IndexedRandom;
 use serde::Deserialize;
+use std::process::{Command, exit};
 use std::time::Duration;
-use std::{env, fs, process};
+use std::{env, fs};
 use ureq::Agent;
 
 const DEFAULT_JOKES: &str = include_str!("jokes.txt");
@@ -14,21 +15,38 @@ struct CommitResponse {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let commit_msg_path = match args.get(1) {
-        Some(path) => path,
-        None => process::exit(0),
-    };
+    let commit_msg = generate_message();
 
-    let final_msg = if rand::random::<f32>() < 0.5 {
+    if let Some(commit_msg_path) = args.get(1) {
+        if let Err(e) = fs::write(commit_msg_path, &commit_msg) {
+            eprintln!("Failed to write commit message: {}", e);
+            exit(1);
+        }
+    } else {
+        let status = Command::new("git")
+            .arg("commit")
+            .arg("-m")
+            .arg(&commit_msg)
+            .status();
+        if let Ok(s) = status {
+            if !s.success() {
+                eprintln!("Git commit failed. Make sure you have staged changes.");
+                exit(1);
+            }
+        } else {
+            exit(1);
+        }
+    }
+}
+
+fn generate_message() -> String {
+    if rand::random::<f32>() < 0.5 {
         fetch_api_msg().ok()
     } else {
         None
     }
     .or_else(fetch_local_backup)
-    .unwrap_or_else(|| "I have no idea what I'm doing.".to_string());
-    if let Err(_) = fs::write(commit_msg_path, final_msg) {
-        process::exit(1);
-    }
+    .unwrap_or_else(|| "I have no idea what I'm doing.".to_string())
 }
 
 fn fetch_api_msg() -> Result<String, Box<dyn std::error::Error>> {
